f597fbd150ecc30a484ba8004258a32cea6a08da
Carlos G. Gavidia <cgavidiac@gmail.com>
2018-10-16 16:24:14 +0100

    Correcting file names

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 Configuration parameters:
 """
 
-SURVEY_DIR = "D:/OneDrive/phd2/priority_survey/"
+SURVEY_DIR = "/Users/carlosgavidiacalderon/Google Drive/phd2/priority_survey/"
 APACHE_FILE = "apache-Survey_ Bug Report Quality.csv"
-ENGLISH_FILE = "federica-Bug Report Quality Survey.csv"
+ENGLISH_FILE = "social-Bug Report Quality Survey.csv"
 SPANISH_FILE = "peru-Encuesta sobre Reporte de Errores.csv"
